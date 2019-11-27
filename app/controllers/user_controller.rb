df57feee1e9487e6c718319fd134a4fc41780cdd
fixel92 <goodzoon92@gmail.com>
2019-11-27 17:32:44 +0400

    Initial commit

---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,5 @@
+class UserController < ApplicationController
+  def show
+    @hello = 'Привет, пользователь!'
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,3 @@
+Rails.application.routes.draw do
+  get 'show' => 'user#show'
+end
