06751ceb46cf74bff7a404d2886d795c3bfc3c43
fixel92 <goodzoon92@gmail.com>
2019-12-02 23:43:03 +0400

    created helper questions count

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,13 @@ module ApplicationHelper
       asset_path 'avatar.jpg'
     end
   end
+
+  def inclination(count, words)
+    word = words[2]
+    word = words[1] if (2..4).include?(count % 10)
+    word = words[0] if (11..14).include?(count % 100) ||
+                       [0, *(5..9)].include?(count % 10)
+
+    "#{count} #{word}"
+  end
 end
