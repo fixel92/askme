a05274bf3b4aa2091fbd5ef8b7c0a5fb071b9a7d
fixel92 <goodzoon92@gmail.com>
2019-12-04 21:18:25 +0400

    updated methods create, update in controller users, added privates methods and before action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+
+  before_action :load_user, except: [:index, :new, :create]
+
   def index
   @users = User.all
   end
@@ -10,16 +13,35 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
 
-    @user.save
+    if @user.save
+      redirect_to root_url, notice: 'Пользователь успешно зарегестрирован'
+    else
+      render 'new'
+    end
   end
 
   def edit
   end
 
+  def update
+    if @user.update(user_params)
+      redirect_to user_path(@user), notice: 'Пользователь успешно обновлен'
+    else
+      render 'edit'
+    end
+  end
+
   def show
+    @questions = @user.questions.order(created_at: :desc)
+
+    @new_question = @user.questions.build
   end
 
   private
+
+  def load_user
+    @user ||= User.find params[:id]
+  end
 
   def user_params
     params.require(:user).permit(:email, :password, :password_confirmation,
