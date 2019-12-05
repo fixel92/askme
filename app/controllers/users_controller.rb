51cfaf0a1b007865434cd312b591997161b0e61d
fixel92 <goodzoon92@gmail.com>
2019-12-05 16:54:00 +0400

    added authentication and authorization users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,19 @@
 class UsersController < ApplicationController
 
   before_action :load_user, except: [:index, :new, :create]
+  before_action :authorize_user, except: [:index, :new, :create, :show]
 
   def index
   @users = User.all
   end
 
   def new
+    redirect_to root_path, alert: 'Вы уже залогинены' if current_user.present?
     @user = User.new
   end
 
   def create
+    redirect_to root_path, alert: 'Вы уже залогинены' if current_user.present?
     @user = User.new(user_params)
 
     if @user.save
@@ -38,6 +41,10 @@ class UsersController < ApplicationController
   end
 
   private
+
+  def authorize_user
+    reject_user unless @user == current_user
+  end
 
   def load_user
     @user ||= User.find params[:id]
