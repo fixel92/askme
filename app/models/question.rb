297e76ceca0854cc0ee7d9064bce66d80ea712b6
fixel92 <goodzoon92@gmail.com>
2019-12-09 15:17:20 +0400

    added relationship question to user

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,6 +1,7 @@
 class Question < ApplicationRecord
 
   belongs_to :user
+  belongs_to :author, class_name: 'User'
 
   validates :text, :user, presence: true
   validates :text, presence: true, length: { maximum: 255 }
