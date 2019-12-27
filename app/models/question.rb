40fc0606d5f126c7d7bc199503908d0d6370b297
fixel92 <goodzoon92@gmail.com>
2019-12-27 19:26:38 +0300

    added validations tag and create before save question

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,9 +1,30 @@
 class Question < ApplicationRecord
 
+  TAGPATTERN = /#[[:word:]-]+/u
+
   belongs_to :user
   belongs_to :author, class_name: 'User'
   has_and_belongs_to_many :tags
 
   validates :text, :user, presence: true
   validates :text, presence: true, length: { maximum: 255 }
+
+  before_save :add_tags
+  before_update :add_tags
+
+  private
+
+  def add_tags
+    tags.clear
+    tags << create_tags
+  end
+
+  def create_tags
+    tags = text.scan(TAGPATTERN)
+    tags += answer.scan(TAGPATTERN) unless answer.nil?
+    tags.map(&:downcase).uniq.map do |item|
+      Tag.create_or_find_by(text: item.encode('EUC-JP'))
+    end
+  end
+
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -2,4 +2,12 @@ class Tag < ApplicationRecord
   has_and_belongs_to_many :questions
 
   validates :text, presence: true, format: /#[[:word:]-]+/e
+
+  before_validation :downcase_tag
+
+  private
+
+  def downcase_tag
+    text.downcase!
+  end
 end
